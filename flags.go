573fb06d664eef0d14c372a561c31a15c3bbf0bb
zikwall <andrey.kapitonov.96@gmail.com>
2020-08-21 15:19:17 +0300

    Тест нагрузки

--- flags.go
+++ flags.go
@@ -74,6 +74,6 @@ var CliFlags = []cli.Flag{
 	&cli.IntFlag{
 		Name:  "worker-count",
 		Usage: "Максимальное количество обработчиков логов и отправителей в Influx",
-		Value: 17,
+		Value: 20,
 	},
 }
