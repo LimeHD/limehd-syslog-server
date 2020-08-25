9e4b5e1603cbee32ebb3b1e32e32f7ee51d64333
zikwall <andrey.kapitonov.96@gmail.com>
2020-08-25 09:44:44 +0300

    Обработка ошибок в отдельных тредах

--- lib/pool.go
+++ lib/pool.go
@@ -7,22 +7,28 @@ import (
 
 type (
 	Pool struct {
-		pool     chan Receiver
-		taskPool chan func() (Receiver, error)
-		listener func(q Receiver)
-		receiver func(p format.LogParts) (Receiver, error)
-		workers  int
-		senders  int
-		workerFn func(pool *Pool, channel syslog.LogPartsChannel)
+		pool          chan Receiver
+		taskPool      chan func() (Receiver, error)
+		errorPool     chan error
+		listener      func(q Receiver) error
+		receiver      func(p format.LogParts) (Receiver, error)
+		errorHandler  func(err error)
+		workers       int
+		senders       int
+		errorHandlers int
+		workerFn      func(pool *Pool, channel syslog.LogPartsChannel)
 	}
 	PoolConfig struct {
-		ListenerCallback func(q Receiver)
-		ReceiverCallback func(p format.LogParts) (Receiver, error)
-		PoolSize         int
-		WorkerPoolSize   int
-		WorkersCount     int
-		SenderCount      int
-		WorkerFn         func(pool *Pool, channel syslog.LogPartsChannel)
+		ListenerCallback    func(q Receiver) error
+		ReceiverCallback    func(p format.LogParts) (Receiver, error)
+		ErrorHandleCallback func(err error)
+		PoolSize            int
+		WorkerPoolSize      int
+		ErrorPoolSize       int
+		WorkersCount        int
+		SenderCount         int
+		ErrorHandlerCount   int
+		WorkerFn            func(pool *Pool, channel syslog.LogPartsChannel)
 	}
 	Receiver struct {
 		Parser Log
@@ -34,8 +40,11 @@ func NewPool(c PoolConfig) *Pool {
 	p := new(Pool)
 	p.pool = make(chan Receiver, c.WorkerPoolSize)
 	p.taskPool = make(chan func() (Receiver, error), c.PoolSize)
+	p.errorPool = make(chan error, c.ErrorPoolSize)
 	p.listener = c.ListenerCallback
 	p.receiver = c.ReceiverCallback
+	p.errorHandler = c.ErrorHandleCallback
+	p.errorHandlers = c.ErrorHandlerCount
 	p.workers = c.WorkersCount
 	p.senders = c.SenderCount
 	p.workerFn = c.WorkerFn
@@ -49,7 +58,7 @@ func (p *Pool) SetReceiverCallback(c func(p format.LogParts) (Receiver, error)) 
 	p.receiver = c
 }
 
-func (p *Pool) SetListenerCallback(c func(q Receiver)) {
+func (p *Pool) SetListenerCallback(c func(q Receiver) error) {
 	p.listener = c
 }
 
@@ -72,15 +81,24 @@ func (p Pool) listen() {
 	for i := 0; i < p.workers; i++ {
 		go p.sendManager()
 	}
+	for i := 0; i < p.errorHandlers; i++ {
+		go p.errorManager()
+	}
 }
 
 func (p Pool) send(r Receiver) {
 	p.pool <- r
 }
 
+func (p Pool) error(err error) {
+	p.errorPool <- err
+}
+
 func (p Pool) sendManager() {
 	for log := range p.pool {
-		p.listener(log)
+		if err := p.listener(log); err != nil {
+			p.error(err)
+		}
 	}
 }
 
@@ -88,6 +106,14 @@ func (p Pool) taskManager() {
 	for task := range p.taskPool {
 		if receive, err := task(); err == nil {
 			p.send(receive)
+		} else {
+			p.error(err)
 		}
+	}
+}
+
+func (p Pool) errorManager() {
+	for err := range p.errorPool {
+		p.errorHandler(err)
 	}
 }
